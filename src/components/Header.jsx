3deb7d88f0d26e2eaaedb51afb8ce917d9c53d46
Noemi <noemipb7@gmail.com>
2023-02-11 22:23:31 +0100

    proyecto final de citas de pacientes veterinaria

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+
+    return (
+    <>
+        <h1 className="text-orange-900 uppercase font-black text-5xl text-center md:w-2/3 mx-auto">
+            Seguimiento Pacientes {""}
+            <br />
+            <span className="text-orange-600">Veterinaria</span>
+        </h1>
+       
+    </>
+    )
+}
+
+export default Header;
